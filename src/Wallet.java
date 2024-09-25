9b5f36251c61f5734a0229490b77ce3d8d1b5a51
Julian97s <124013438+Julian97s@users.noreply.github.com>
2024-09-24 21:31:36 -0400

    Up to bouncy castle implemented

---
+++ src/Wallet.java
@@ -0,0 +1,32 @@
+package src;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.ECGenParameterSpec;
+
+public class Wallet {
+    public PrivateKey privateKey;
+    public PublicKey publicKey;
+
+    public Wallet(){
+        generateKeyPair();
+    }
+
+    public void generateKeyPair(){
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA","BC");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+            // initialize the key generator and generate a keyPair
+            keyGen.initialize(ecSpec,random); //256 bytes = aceptable security level
+            KeyPair keyPair = keyGen.generateKeyPair();
+            // set public and private keys
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
